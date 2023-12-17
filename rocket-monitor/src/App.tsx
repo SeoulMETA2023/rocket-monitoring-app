6270b8b19286a8a1fb0863994dd03ea852bbe1e7
piop2 <seungyounyou0220@gmail.com>
2023-12-18 00:51:50 +0900

    add socket.tsx

--- rocket-monitor/src/App.tsx
+++ rocket-monitor/src/App.tsx
@@ -1,22 +1,37 @@
-// import {Fragment} from "react";
-
-// import io from "socket.io-client"
+import React, {Fragment, useEffect, useState} from "react";
 
 import RCSMonitor from "./monitor/rcs";
 import CandyMonitor from "./monitor/candy";
-import {Fragment} from "react";
+import {socket} from "./socket";
 
 function App() {
+    const [isConnected, setIsConnected] = useState<boolean>(socket.connected);
+
+    useEffect(() => {
+        function onConnect() {
+            setIsConnected(true);
+        }
+
+        function onDisconnect() {
+            setIsConnected(false);
+        }
+
+        return () => {
+            socket.off("connect", onConnect)
+            socket.off("disconnect", onDisconnect)
+        }
+    }, [])
+
     return (
         <Fragment>
-            {/*<div className={"w-full h-screen bg-gear-black grid place-items-center absolute duration-500 z-10"}>*/}
-            {/*    <div className={"w-52 flex flex-col items-center gap-7"}>*/}
-            {/*        <img src={"meta_icon(white).svg"} alt={"meta logo"} className={"w-full animate-loading"}/>*/}
-            {/*        <div className={"text-white font-bold text-2xl font-noto-sans"}>*/}
-            {/*            Connecting...*/}
-            {/*        </div>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
+            <div className={`w-full h-screen bg-gear-black grid place-items-center absolute duration-500 z-10 ${isConnected? "opacity-0" : "opacity-100"}`}>
+                <div className={"w-52 flex flex-col items-center gap-7"}>
+                    <img src={"meta_icon(white).svg"} alt={"meta logo"} className={"w-full animate-loading"}/>
+                    <div className={"text-white font-bold text-2xl font-noto-sans"}>
+                        Connecting...
+                    </div>
+                </div>
+            </div>
             <div className={"h-screen bg-gear-black grid grid-cols-2"}>
                 <RCSMonitor/>
                 <CandyMonitor/>
