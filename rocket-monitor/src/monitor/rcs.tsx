f5812454bdad3c28b207349f6cb00bb3558b30a6
piop2 <seungyounyou0220@gmail.com>
2023-12-18 00:12:23 +0900

    add line graph

--- rocket-monitor/src/monitor/rcs.tsx
+++ rocket-monitor/src/monitor/rcs.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Terminal, {Message} from "./terminal"
 import Visualizer from "./visualizer";
+import {StatusLine} from "./statusGraph";
 
 export default function RCSMonitor() {
 
@@ -23,7 +24,7 @@ export default function RCSMonitor() {
             </div>
 
             {/* terminal */}
-            <div className={"col-span-5 row-span-3"}>
+            <div className={"col-span-5 row-span-3 bg-dim-gray rounded-xl"}>
                 <Terminal>
                     <Message timestamp={1702512900} content={"hi"}/>
                 </Terminal>
@@ -31,36 +32,42 @@ export default function RCSMonitor() {
 
             {/* gyro 1 */}
             <div className={"col-span-4 row-span-2 bg-dim-gray rounded-xl"}>
-
+                <StatusLine title={"GYRO X"} dataSets={[-800, 0, 600, 400, 300, -300, 100]} y={{max: 1000, min: -1000, step: 500}} color={"#FF6384"}/>
             </div>
 
             {/* gyro 2 */}
             <div className={"col-span-4 row-span-2 bg-dim-gray rounded-xl"}>
+                <StatusLine title={"GYRO Y"} dataSets={[-800, 0, 600, 400, 300, -300, 100]} y={{max: 1000, min: -1000, step: 500}} color={"#FF6384"}/>
 
             </div>
 
             {/* gyro 3 */}
             <div className={"col-span-4 row-span-2 bg-dim-gray rounded-xl"}>
+                <StatusLine title={"GYRO Z"} dataSets={[-800, 0, 600, 400, 300, -300, 100]} y={{max: 1000, min: -1000, step: 500}} color={"#FF6384"}/>
 
             </div>
 
             {/* rcs 1 */}
             <div className={"col-span-3 row-span-2 bg-dim-gray rounded-xl"}>
+                <StatusLine title={"RCS 1"} dataSets={[0, 0, 1, 0, 1, 1, 1]} y={{max: 1, min: 0, step: 1}} color={"#FF6384"}/>
 
             </div>
 
             {/* rcs 2 */}
             <div className={"col-span-3 row-span-2 bg-dim-gray rounded-xl"}>
+                <StatusLine title={"RCS 2"} dataSets={[0, 0, 1, 0, 1, 1, 1]} y={{max: 1, min: 0, step: 1}} color={"#FF6384"}/>
 
             </div>
 
             {/* rcs 3 */}
             <div className={"col-span-3 row-span-2 bg-dim-gray rounded-xl"}>
+                <StatusLine title={"RCS 3"} dataSets={[0, 0, 1, 0, 1, 1, 1]} y={{max: 1, min: 0, step: 1}} color={"#FF6384"}/>
 
             </div>
 
             {/* rcs 4 */}
             <div className={"col-span-3 row-span-2 bg-dim-gray rounded-xl"}>
+                <StatusLine title={"RCS 4"} dataSets={[0, 0, 1, 0, 1, 1, 1]} y={{max: 1, min: 0, step: 1}} color={"#FF6384"}/>
 
             </div>
         </section>
---
+++ rocket-monitor/src/monitor/statusGraph.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+);
+
+interface StatusLineProps {
+    title: string
+    y: {
+        max: number
+        min: number
+        step: number
+    }
+    dataSets: number[]
+    color: string
+}
+
+export function StatusLine(props: StatusLineProps) {
+    const options = {
+        maintainAspectRatio: false,
+        responsive: true,
+        plugins: {
+            title: {
+                display: true,
+                text: props.title,
+                color: "#FFFFFF"
+            },
+        },
+        scales: {
+            y: {
+                min: props.y.min,
+                max: props.y.max,
+                ticks: {
+                    color: "#FFFFFF",
+                    stepSize: props.y.step
+                }
+            },
+        }
+    };
+
+    const labels = Array.from(props.dataSets, () => "");
+
+    const data = {
+        labels,
+        datasets: [
+            {
+                label: 'Dataset 1',
+                data: props.dataSets,
+                borderColor: `${props.color}7F`,
+                backgroundColor: props.color,
+            }
+        ],
+    };
+
+    return (
+        <Line options={options} data={data} className={"bg-gear-black border-dim-gray border-2 rounded-xl"}/>
+    );
+}
