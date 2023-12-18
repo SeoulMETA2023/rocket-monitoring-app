e968a631cd624d7cf49d51e86f0b619eb29e513a
piop2 <seungyounyou0220@gmail.com>
2023-12-18 10:21:05 +0900

    resize loading screen image

--- rocket-monitor/src/App.tsx
+++ rocket-monitor/src/App.tsx
@@ -28,7 +28,7 @@ function App() {
     return (
         <Fragment>
             <div className={`w-full h-screen bg-gear-black grid place-items-center absolute duration-500 z-10 ${isConnected? "invisible" : "visible"}`}>
-                <div className={"w-52 flex flex-col items-center gap-7"}>
+                <div className={"w-44 flex flex-col items-center gap-7"}>
                     <img src={"meta_icon(white).svg"} alt={"meta logo"} className={"w-full animate-loading"}/>
                     <div className={"text-white font-bold text-2xl font-noto-sans"}>
                         Connecting...
